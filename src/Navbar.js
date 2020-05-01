5e38be89b18e05640b82187920f6c35cbaaf916d
HARSH JAIN <jainharsh270@gmail.com>
2020-05-01 23:29:37 +0530

    select component changes the format from hex to rgb or rgba depending on val selected

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,8 +6,18 @@ import "./Navbar.css";
 import { MenuItem } from "@material-ui/core";
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { format: "hex" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ format: e.target.value });
+    this.props.handleChange(e.target.value);
+  }
   render() {
     const { level, changeLevel } = this.props;
+    const { format } = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
@@ -26,7 +36,7 @@ class Navbar extends Component {
           </div>
         </div>
         <div className="select-container">
-          <Select>
+          <Select value={format} onChange={this.handleChange}>
             <MenuItem value="hex"> HEX - #ffffff </MenuItem>
             <MenuItem value="rgb"> RGB - rgb(255,255,255) </MenuItem>
             <MenuItem value="rgba"> RGBA - rgba(255,255,255,1.0) </MenuItem>
