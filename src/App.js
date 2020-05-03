1b0485ab8a32c353870e88f9f44dd16857accb0f
HARSH JAIN <jainharsh270@gmail.com>
2020-05-03 19:36:05 +0530

    palette route working

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React, { Component } from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
-import { Route , Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function (palette) {
+      return palette.id === id;
+    });
+  }
   render() {
     console.log(generatePalette(seedColors[4]));
     return (
       <Switch>
         <Route exact path="/" render={() => <h1>Palette List Goes Here!</h1>} />
-        <Route exact path="/palette/:id" render={() => <h1>Individual Palette!</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={(routeProps) => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
       </Switch>
       // <div>
       //   <Palette palette={generatePalette(seedColors[4])} />
