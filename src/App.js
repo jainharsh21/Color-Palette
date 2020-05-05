b2534e4fa48375544f48472a0d3a9f443571dea3
HARSH JAIN <jainharsh270@gmail.com>
2020-05-05 12:03:13 +0530

    added link to SingleColorPalette

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 import { Route, Switch } from "react-router-dom";
 import PaletteList from "./PaletteList";
+import SingleColorPalette from "./SingleColorPalette";
 
 class App extends Component {
   findPalette(id) {
@@ -36,7 +37,7 @@ class App extends Component {
         <Route
           exact
           path="/palette/:paletteId/:colorId"
-          render={() => <h1>individual color palette</h1>}
+          render={() => <SingleColorPalette />}
         />
       </Switch>
       // <div>
