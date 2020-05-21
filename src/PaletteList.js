9b972f32aadb6238dc5c8a7ece701267329a7ccc
HARSH JAIN <jainharsh270@gmail.com>
2020-05-21 12:45:17 +0530

    added create palette link and styled it

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import MiniPalette from "./MiniPalette";
 import { withStyles } from "@material-ui/styles";
-import styles from "./styles/PaletteListStyles"; 
+import styles from "./styles/PaletteListStyles";
+import { Link } from "react-router-dom";
 
 class PaletteList extends Component {
   goToPalette(id) {
@@ -15,6 +16,7 @@ class PaletteList extends Component {
         <div className={classes.container}>
           <nav className={classes.nav}>
             <h1>React Colors!</h1>
+            <Link to="/palette/new">Create Palette</Link>
           </nav>
           <div className={classes.palettes}>
             {palettes.map((palette) => (
