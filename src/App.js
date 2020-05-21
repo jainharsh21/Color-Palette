68ba7af2bbbcddf66176d1fb56c687b8d5dc689c
HARSH JAIN <jainharsh270@gmail.com>
2020-05-21 12:40:59 +0530

    added component for new palette form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { generatePalette } from "./colorHelpers";
 import { Route, Switch } from "react-router-dom";
 import PaletteList from "./PaletteList";
 import SingleColorPalette from "./SingleColorPalette";
+import NewPaletteForm from "./NewPaletteForm";
 
 class App extends Component {
   findPalette(id) {
@@ -16,11 +17,7 @@ class App extends Component {
     console.log(generatePalette(seedColors[4]));
     return (
       <Switch>
-        <Route
-          exact
-          path="/palette/new"
-          render={() => <h1>New Palette Form!</h1>}
-        />
+        <Route exact path="/palette/new" render={() => <NewPaletteForm />} />
         <Route
           exact
           path="/"
